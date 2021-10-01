59b135d0fe5e472792ad52e0c49c6af08b87ef81
Pablo Sanderson <pablosr11@gmail.com>
2021-10-02 00:23:34 +0100

    Add file download workflow

--- fuploader/src/index.ts
+++ fuploader/src/index.ts
@@ -11,8 +11,20 @@ app.get("/", (req, res) => {
   res.render("/Users/pablo.sanderson/repos/myheritage/fuploader/index.html");
 });
 
-app.get("/result", (req, res) => {
+app.get("/result/:jobId", (req, res) => {
+  console.log(`Downloading ${req.params["jobId"]}`);
   res.download("/Users/pablo.sanderson/repos/myheritage/fuploader/TODO");
+});
+
+let isready = false;
+app.get("/status/:jobId", (req, res) => {
+  // check work is ready, if not update field
+  if (isready) {
+    res.json({ link: `http://localhost:8080/result/${req.params["jobId"]}` });
+  } else {
+    res.json({ msg: "not yet" });
+    isready = true;
+  }
 });
 
 app.post("/upload", (req, res) => {
@@ -22,16 +34,14 @@ app.post("/upload", (req, res) => {
   req.on("data", (chunk) => {
     // check is the expected chunk, otherwise corrupted file
     fs.appendFileSync(filename, chunk);
-    console.log(`received ${chunk.length}`);
+    console.log(`received ${chunk.length}. Storing in ${filename}`);
   });
-
-  console.log(`stored in ${name}`);
 
   // check if final header included, and filesize is equal to it
   if (req.query["q"] && +req.query["q"] === fs.statSync(filename).size) {
     //trigger whatever comes next
     console.log("COMMMMPLETEDDDDD");
-    res.json({ link: `http://localhost:8080/result/?i=${name}` });
+    res.json({ link: `http://localhost:8080/status/${name}` });
   }
   res.end();
 });
