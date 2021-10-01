d625e2473d4c741d2e07f82b44ba3ec0a5d084cd
Pablo Sanderson <pablosr11@gmail.com>
2021-10-02 00:38:40 +0100

    Small refactor to express app

--- fuploader/src/index.ts
+++ fuploader/src/index.ts
@@ -1,18 +1,22 @@
 import express from "express";
-
-const app = express();
-app.engine("html", require("ejs").renderFile);
-const port = 8080; // default port to listen
-
 import fs from "fs";
 
-// define a route handler for the default home page
+const app = express();
+const port = 8080; // default port to listen
+app.engine("html", require("ejs").renderFile);
+
+const HOST = `http://localhost:${port}`;
+
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at ${HOST}`);
+});
+
 app.get("/", (req, res) => {
   res.render("/Users/pablo.sanderson/repos/myheritage/fuploader/index.html");
 });
 
 app.get("/result/:jobId", (req, res) => {
-  console.log(`Downloading ${req.params["jobId"]}`);
   res.download("/Users/pablo.sanderson/repos/myheritage/fuploader/TODO");
 });
 
@@ -20,9 +24,9 @@ let isready = false;
 app.get("/status/:jobId", (req, res) => {
   // check work is ready, if not update field
   if (isready) {
-    res.json({ link: `http://localhost:8080/result/${req.params["jobId"]}` });
+    res.json({ link: `${HOST}/result/${req.params["jobId"]}` });
   } else {
-    res.json({ msg: "not yet" });
+    res.json({ isready: 0 });
     isready = true;
   }
 });
@@ -32,21 +36,15 @@ app.post("/upload", (req, res) => {
   let filename = name + ".csv";
 
   req.on("data", (chunk) => {
-    // check is the expected chunk, otherwise corrupted file
+    // add ID to identify each chunk - to detect missing packets
     fs.appendFileSync(filename, chunk);
     console.log(`received ${chunk.length}. Storing in ${filename}`);
   });
 
   // check if final header included, and filesize is equal to it
   if (req.query["q"] && +req.query["q"] === fs.statSync(filename).size) {
-    //trigger whatever comes next
-    console.log("COMMMMPLETEDDDDD");
-    res.json({ link: `http://localhost:8080/status/${name}` });
+    console.log(`Upload completed: ${filename}`);
+    res.json({ link: `${HOST}/status/${name}` });
   }
   res.end();
-});
-
-// start the Express server
-app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
 });
